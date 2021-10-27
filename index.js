d97758c4282d9eeb0bef0110706027fb1d93754c
NiiOdai <mkookley-mensah@st.ug.edu.gh>
2021-10-27 06:41:13 +0000

    Bootstrapped Server Successful

---
+++ index.js
@@ -0,0 +1,58 @@
+const {ApolloServer, gql} = require("apollo-server");
+
+// const express = require('express');
+
+// const app =express();
+// app.listen (4000,() => {
+//     console.log("Server started on port 4000");
+// })
+//Schemas
+//typeDefs
+const books = [
+    {
+        title:"Harry Potter and the Chamber of Secrets",
+        author:"J.K. Rowling",
+        ISBN:"0-7475-3269-9"
+    },
+    {
+        title:"Jurassic Park",
+        author:"Michael Crichton",
+        ISBN: "0-7475-3269-9"
+    }
+];
+//type NameOfthe type
+
+const schemas = gql`
+
+    type Book {
+        title: String!
+        author: String!
+        ISBN: String
+    }
+    type Query {   
+            books:[Book]
+            book(title:String!):Book
+        }
+    type Mutation {
+        createBook(title: String!, author: String!,ISBN: String): Book
+    }
+
+`;
+const booksResolvers = {
+    Query:{
+        books:() => books,
+        book: (parent,args) => books.find(book => book.title ==args.title)
+    },
+    Mutation:{
+        createBook: (parent,args) =>{
+            const {title, author,ISBN} = args;
+            const book = {title, author,ISBN };
+            books.push(book);
+            return book;
+        }
+    },
+}
+const server = new ApolloServer({ typeDefs: schemas,resolvers: booksResolvers});
+server.listen(8000).then(({url,port}) => {
+    console.log(`Server ready at ${url}`);
+}).catch(err => console.log(err));
