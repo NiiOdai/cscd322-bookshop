6c551d8acf0f7f962ae275560a6b117f53606c4c
NiiOdai <mkookley-mensah@st.ug.edu.gh>
2021-10-27 08:02:59 +0000

    Added PLayground and introspection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {ApolloServer, gql} = require("apollo-server");
-const port = process.env.PORT||8000;
+const port =process.env.PORT || 8000;
+
 // const express = require('express');
 
 // const app =express();
@@ -38,7 +39,7 @@ const schemas = gql`
     }
 
 `;
-const booksResolvers ={
+const booksResolvers = {
     Query:{
         books:() => books,
         book: (parent,args) => books.find(book => book.title ==args.title)
@@ -52,7 +53,10 @@ const booksResolvers ={
         }
     },
 }
-const server = new ApolloServer({ typeDefs: schemas,resolvers: booksResolvers});
-server.listen(port).then(({url,port}) => {
-    console.log(`Server ready at ${url}`);
-}).catch(err => console.log(err));
+const server = new ApolloServer({ typeDefs: schemas,resolvers: booksResolvers,
+playground:true,
+introspection:true});
+
+server.listen(port).then(({url}) => {
+    console.log(`Server ready at ${url} and ready to be used`);
+}).catch(err => console.log(err.message));
